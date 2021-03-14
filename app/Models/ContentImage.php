9f45986fbc73e7b722a2d2f211e31dc5ee65b116
e.skrilnikov <e.skrilnikov@artjoker.team>
2021-03-14 18:10:38 +0200

    fixes

--- app/Models/ContentImage.php
+++ app/Models/ContentImage.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class ContentImage extends Model
@@ -22,6 +23,7 @@ class ContentImage extends Model
 
     public function getResizeImageUrl(string $path = '')
     {
+        Log::info('$this->name: ', [$this->name, $this->original_name]);
         return Storage::disk('public')->has('storage/images/resize_images/' . $path . DIRECTORY_SEPARATOR . $this->name)
             ? asset('storage/images/resize_images/' . $path . DIRECTORY_SEPARATOR . $this->name)
             : asset('storage/images/upload_images/' . $path . DIRECTORY_SEPARATOR . $this->original_name);
