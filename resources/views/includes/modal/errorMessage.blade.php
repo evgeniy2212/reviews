edb2c59553422b5aa594ef3cd8d03a219093a380
e.skrilnikov <e.skrilnikov@artjoker.team>
2022-05-19 21:10:33 +0300

    chat fx

--- resources/views/includes/modal/errorMessage.blade.php
+++ resources/views/includes/modal/errorMessage.blade.php
@@ -1,15 +1,16 @@
 <div class="modal fade errorModalDialog" id="errorMessage" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
+            <span id="errorMessageContent" hidden></span>
             @if($errors->any())
                 <span>{!! $errors->first() !!}</span>
             @else
-                <span>Error</span>
+                <span id="defaultErrorMessageContent">@lang('service/index.error')</span>
             @endif
             <div class="d-flex justify-content-center">
                 <div class="col-md-3">
                     <button class="otherButton" type="button" data-dismiss="modal">
-                        Cancel
+                        @lang('service/index.cancel')
                     </button>
                 </div>
             </div>
--- resources/views/includes/modal/successMessage.blade.php
+++ resources/views/includes/modal/successMessage.blade.php
@@ -1,15 +1,16 @@
 <div class="modal fade successModalDialog" id="successMessage" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
+            <span id="successMessageContent" hidden></span>
             @if(session()->has('success'))
                 @foreach (session()->get('success') as $message)
-                    <span>{!! $message !!}</span>
+                    <span class="successMessageContent">{!! $message !!}</span>
                 @endforeach
             @endif
             <div class="d-flex justify-content-center">
                 <div class="col-md-3">
                     <button class="otherButton" type="button" data-dismiss="modal">
-                        Cancel
+                        @lang('service/index.cancel')
                     </button>
                 </div>
             </div>
