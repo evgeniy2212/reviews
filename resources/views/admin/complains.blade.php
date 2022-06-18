401173ce0e2f4208018acdc613f166b5c25b43d6
e.skrilnikov <e.skrilnikov@artjoker.team>
2022-06-18 17:04:18 +0300

    fx complains

--- resources/views/admin/complains.blade.php
+++ resources/views/admin/complains.blade.php
@@ -8,8 +8,9 @@
                     <a class="admin-complain {{ (array_key_exists('is_new', $paginateParams) && $paginateParams['is_new'] === '1')
                         ? 'active'
                         : ''}}"
-                       name="is_new"
-                       value="1">@lang('service/index.new') ({!! $newReviewsCnt !!})</a>
+                       href="{{ route('admin.complains.index', ['is_new' => 1]) }}">
+                        @lang('service/index.new') ({!! $newReviewsCnt !!})
+                    </a>
                 </div>
             </div>
             <div class="adminFilterItem">
@@ -17,8 +18,9 @@
                     <a class="admin-complain {{ (array_key_exists('is_new', $paginateParams) && $paginateParams['is_new'] === '0')
                                             ? 'active'
                                             : ''}}"
-                       name="is_new"
-                       value="0">@lang('service/index.processed') ({!! $processedReviewsCnt !!})</a>
+                       href="{{ route('admin.complains.index', ['is_new' => 0]) }}">
+                        @lang('service/index.processed') ({!! $processedReviewsCnt !!})
+                    </a>
                 </div>
             </div>
             <div class="adminFilterItem">
@@ -26,8 +28,9 @@
                     <a class="admin-complain {{ ((array_key_exists('is_new', $paginateParams) && $paginateParams['is_new'] === 'NULL') || !array_key_exists('is_new', $paginateParams))
                                             ? 'active'
                                             : ''}}"
-                       name="is_new"
-                       value="NULL">@lang('service/index.all') ({!! $allReviewsCnt !!})</a>
+                       href="{{ route('admin.complains.index', ['is_new' => null]) }}">
+                        @lang('service/index.all') ({!! $allReviewsCnt !!})
+                    </a>
                 </div>
             </div>
         </div>
