5428486d551170e717d3a9ddfed0b4266578dadc
e.skrilnikov <e.skrilnikov@artjoker.team>
2022-10-05 20:01:39 +0300

    chat fx

--- resources/views/mails/chat_approving.blade.php
+++ resources/views/mails/chat_approving.blade.php
@@ -1,8 +1,8 @@
 @component('mail::message')
 
-@lang('service/message.mail.hi') {{ $contact_name }}.
+@lang('service/message.mail.hi'), {{ $contact_name }}!
 
-{{ config('app.name') }}, inform you that {{ $name }} want to include you in his list of contact to chat with you in real time in the future. If you agree click the button below.
+{{ $name }} wants to add you to their contacts so they can chat with you at {{ config('app.name') }}! If you agree, click the button below.
 
 @component('mail::button', ['url' => $url])
     @lang('service/message.mail.agree')
